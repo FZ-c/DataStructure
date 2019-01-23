ef13be5eb9c76786098eea29bfcf39413f2134d5
FZ-c <fz-c@outlook.com>
2019-01-23 20:54:29 +0800

    完善线性表(v0.2)

--- test.cpp
+++ test.cpp
@@ -1,11 +1,12 @@
 ﻿#include <iostream>
+#include<vector>
 #include"LinearList.h"
 
 using namespace std;
 
 int main()
 {
-	Array<int>num;
+	Array<int>num, xx;
 	num[0] = 1;
 	for (int *p = num.begin(); p < num.end(); p++)
 		cout << *p << ' ';
@@ -15,11 +16,23 @@ int main()
 		cout << *p << ' ';
 	cout << endl;
 	num.push_back(121);
+	cout << num.length() << endl;
 	num.push_back(120);
+	cout << num.length() << endl;
 	num.push_back(113);
+	cout << num.length() << endl;
 	num.push_back(119);
+	cout << num.length() << endl;
 	for (int *p = num.begin(); p < num.end(); p++)
 		cout << *p << ' ';
+	xx = num;
+
+	cout << xx.length() << endl;
+	for (int *p = xx.begin(); p < xx.end(); p++)
+		cout << *p << ' ';
+	num.clear();
+	xx = num;
+	cout << xx.length() << endl;
 	cout << endl;
 	return 0;
 }
